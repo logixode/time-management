22fad82fe581aa65f32ee5a3c49fb5cdc2c428e7
logixode <rohmad1230@gmail.com>
2020-01-10 11:18:30 +0700

    add addSchedule() function

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ function showMenu() {
     document.getElementById('menu').classList.toggle('menu-mobile')
 }
 
+function addSchedule() {
+    document.getElementById('dialog').classList.toggle('show-dialog')
+    document.body.classList.toggle('oflow-hidden')
+}
+
 const date = new Date();
 document.getElementById("date").innerHTML = date;
